4b5af28f2a09b1d7a474137a58f94ac194ed7b18
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-11 05:04:34 -0300

    [01 - CONCEITOS][AULA 05] - Const & Let

--- bundle.js
+++ bundle.js
@@ -1,20 +1,11 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function teste(x) {
+  var y = 2;
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Teste = /*#__PURE__*/function () {
-  function Teste() {
-    _classCallCheck(this, Teste);
+  if (x > 5) {
+    console.log(x, y);
   }
+}
 
-  _createClass(Teste, [{
-    key: "metodo",
-    value: function metodo() {}
-  }]);
-
-  return Teste;
-}();
+teste(10);
