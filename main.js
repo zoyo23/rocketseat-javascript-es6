11fbeec329da9c8ec5517ee58693ab7c2426feaf
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-11 05:23:46 -0300

    [01 - CONCEITOS][AULA 08] - Valores Padrão

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-const arr = [1, 3, 4, 5, 6];
+const soma = (a = 3, b = 6) => a + b;
 
-const newArr = arr.map(item => item * 2);
-
-console.log(newArr);
-
-const teste = () => ({ nome: "Lucas" });
-
-console.log(teste());
+console.log(soma(1));
+console.log(soma());
