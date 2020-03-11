a18af5b0e413abb9a87f03dfee93ea1efaa36959
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-11 05:43:58 -0300

    [01 - CONCEITOS][AULA 12] - Object Short Syntax

--- bundle.js
+++ bundle.js
@@ -1,6 +1,10 @@
 "use strict";
 
 var nome = "Lucas";
-var idade = 24; // console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
-
-console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos."));
+var idade = 24;
+var usuario = {
+  nome: nome,
+  idade: idade,
+  empresa: "BMG"
+};
+console.log(usuario);
--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const nome = "Lucas";
 const idade = 24;
 
-// console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
-console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
+const usuario = {
+    nome,
+    idade,
+    empresa: "BMG"
+};
+
+console.log(usuario);
