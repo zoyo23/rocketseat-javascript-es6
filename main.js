e05fd327a0cac8d72aa82262a5ddfce1050a4cef
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-11 05:41:31 -0300

    [01 - CONCEITOS][AULA 11] - Template Literals

--- main.js
+++ main.js
@@ -1,48 +1,5 @@
-// REST
+const nome = "Lucas";
+const idade = 24;
 
-const usuario = {
-    nome: "Lucas",
-    idade: 24,
-    empresa: "BMG"
-};
-
-const { nome, ...resto } = usuario;
-
-console.log(nome);
-console.log(resto);
-
-const arr = [1, 2, 3, 4];
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-function soma(...params) {
-    return params.reduce((total, next) => total + next);
-}
-
-function soma2(a, b, ...params) {
-    return params;
-}
-console.log(soma(1, 3, 4));
-console.log(soma2(1, 3, 4));
-
-// SPREAD
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-
-const usuarioSpread = {
-    nome: "Lucas",
-    idade: 24,
-    empresa: "BMG"
-};
-
-const usuarioSpread2 = { ...usuarioSpread, nome: "Carol" };
-
-console.log(usuarioSpread2);
+// console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
