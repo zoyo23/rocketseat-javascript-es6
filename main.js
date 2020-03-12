20ba46ed91e8d3294f1a2bd912a7dfb16a63e689
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-12 05:51:25 -0300

    [02 - WEBPACK SERVER][AULA 03] - Webpack dev server

--- main.js
+++
@@ -1,7 +0,0 @@
-import * as funcoes from './funcoes';
-// import soma, { sub as subtracao } from './funcoes';
-// import soma from './soma';
-
-console.log(funcoes);
-console.log(funcoes.soma(1, 2));
-console.log(funcoes.sub(1, 2));
