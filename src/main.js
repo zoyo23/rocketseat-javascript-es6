0195bb6c041612c9dbd1515a3b0ff58a694d959d
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-17 06:17:28 -0300

    [03 - ASYNC/AWAIT][AULA 01] - Async/await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-alert("OPA xD!");
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('ok') }, 2000);
+});
+
+// minhaPromise()
+//     .then(response => {
+//         console.log(response);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+// async function executaPromise() {
+//     const response = await minhaPromise();
+//     console.log(response);
+// }
+
+const executaPromise = async () => {
+    const response = await minhaPromise();
+    console.log(response);
+}
+
+executaPromise();
