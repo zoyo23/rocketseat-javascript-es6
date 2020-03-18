911c533ba2b7bcf046180d6e5f29bd07477e792c
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-18 05:22:33 -0300

    [04 - APLICAÇÃO EM ES6+][AULA 03] - Render em tela

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
 
         this.formEl = document.getElementById("repo-form");
+        this.listEl = document.getElementById("repo-list");
 
         this.registerHandlers();
     }
@@ -13,13 +14,43 @@ class App {
 
     addRepository(event) {
         event.preventDefault();
+
         this.repositories.push({
             name: 'rocketseat.com.br',
             description: 'Tire a sua ideia do papel e de vida à sua startup.',
             avatar_url: 'http://avatars0.githubusercontent.com/u/28929274?v=4',
             html_url: 'https://github.com/Rocketseat/rocketpwa'
         });
-        console.log(this.repositories);
+
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = "";
+
+        this.repositories.forEach(item => {
+            let imgEl = document.createElement("img");
+            imgEl.setAttribute("src", item.avatar_url);
+
+            let tittleEl = document.createElement("strong");
+            tittleEl.appendChild(document.createTextNode(item.name));
+
+            let descriptionEl = document.createElement("p");
+            descriptionEl.appendChild(document.createTextNode(item.description));
+
+            let linkEl = document.createElement("a");
+            linkEl.setAttribute("target", "_blank");
+            linkEl.setAttribute("href", item.html_url);
+            linkEl.appendChild(document.createTextNode("Acessar"));
+
+            let listItemEl = document.createElement("li");
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(tittleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
